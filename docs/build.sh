453423ae6db5f1e08e5e3c4aa5b5fe83e49cac8c
kenielf <chrystian.keni.github@gmail.com>
2024-10-20 16:10:11 -0300

    Documentation: Created the build script, added the initial part of the technical analysis.

---
+++ docs/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Safeguard - lualatex is mandatory for the compilation of these docs
+if ! command -v lualatex; then
+    exit 1
+fi
+
+# Compile twice (to make sure any tocs or bib tables are built and filled)
+lualatex --shell-escape ./*.tex && \
+    lualatex --shell-escape ./*.tex
+
+# Clean temporary files
