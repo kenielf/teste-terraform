c4a787c68f70065ea7ff8897ec9971304b7d78ab
kenielf <chrystian.keni.github@gmail.com>
2024-10-20 21:28:26 -0300

    Security: limit the ingress/egress permissions.

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_route_table_association" "main_association" {
 
 resource "aws_security_group" "main_sg" {
   name        = "${var.projeto}-${var.candidato}-sg"
-  description = "Permitir SSH de qualquer lugar e todo o tráfego de saída"
+  description = "Permitir SSH somente de endereços autorizados"
   vpc_id      = aws_vpc.main_vpc.id
 
   # Regras de entrada
@@ -84,6 +84,15 @@ resource "aws_security_group" "main_sg" {
     description      = "Allow SSH from anywhere"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["8.8.8.8/32"]  # IP da Google como exemplo somente.
+    ipv6_cidr_blocks = []
+  }
+
+  ingress {
+    description      = "Allow HTTPS traffic"
+    from_port        = 443
+    to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
